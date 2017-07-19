339f47da911fa84d5058b5211952b2d40a350d75
Peter Wilson <pwillie@users.noreply.github.com>
2017-07-20 09:24:14 +1000

    using gorilla/mux for header routing

--- main.go
+++ main.go
@@ -1,36 +1,54 @@
 package main
 
 import (
-	"fmt"
+	"log"
+	"net/http"
 	"strconv"
-	"github.com/namsral/flag"
+	"time"
+	"github.com/gorilla/mux"
+	"github.com/kelseyhightower/envconfig"
 	"gopkg.in/go-playground/webhooks.v3"
 	"gopkg.in/go-playground/webhooks.v3/bitbucket"
 )
 
+type Specification struct {
+	Debug bool    `default:"false"`
+	Port  int     `default:"8080"`
+	UUID  string
+}
+
 func main() {
-	var (
-		port int
-		uuid string
-	)
+	var s Specification
+	err := envconfig.Process("webhook", &s)
+	if err != nil {
+		log.Fatal(err.Error())
+	}
 
-	flag.IntVar(&port, "port", 8080, "TCP listener port")
-	flag.StringVar(&uuid, "uuid", "", "Webhook UUID")
-	flag.Parse()
-
-	hook := bitbucket.New(&bitbucket.Config{UUID: uuid})
+	hook := bitbucket.New(&bitbucket.Config{UUID: s.UUID})
 	hook.RegisterEvents(HandleMultiple, bitbucket.RepoPushEvent) // Add as many as you want
 
-	err := webhooks.Run(hook, ":"+strconv.Itoa(port), "/bitbucket")
-	if err != nil {
-		fmt.Println(err)
+	r := mux.NewRouter()
+	r.Path("/bitbucket").
+		Handler(webhooks.Handler(hook)).
+		Headers("X-Hook-UUID", s.UUID)
+
+	srv := &http.Server{
+		Handler: r,
+		Addr:    ":" + strconv.Itoa(s.Port),
+		// Good practice: enforce timeouts for servers you create!
+		WriteTimeout: 15 * time.Second,
+		ReadTimeout:  15 * time.Second,
 	}
+
+	//log.Printf("Listening on addr: %s path: %s", addr, s.Path)
+	log.Printf("Listening on addr: %s path: %s", ":" + strconv.Itoa(s.Port), "/bitbucket")
+	log.Fatal(srv.ListenAndServe())
 }
 
 // HandleMultiple handles multiple GitHub events
 func HandleMultiple(payload interface{}, header webhooks.Header) {
 
-	fmt.Println("Handling Payload..")
+	log.Println("Handling Payload..")
 
 	switch payload.(type) {
 
@@ -38,6 +56,6 @@ func HandleMultiple(payload interface{}, header webhooks.Header) {
 		release := payload.(bitbucket.RepoPushPayload)
 		// Do whatever you want from here...
 		// TODO: post payload to Jenkins
-		fmt.Printf("%+v", release)
+		log.Printf("%+v", release)
 	}
 }
